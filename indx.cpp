9a05b9c31bda4aa73ed06c1e5c95dc22051f47e8
ZAEEM UL HASSAN <170763155+zaeemulhassan12@users.noreply.github.com>
2026-05-13 23:14:10 +0500

    Implement single number solution in C++
    reslove error or add libaray files etc

--- indx.cpp
+++ indx.cpp
@@ -1,12 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int res = 0;
+
         for (int num : nums) {
             res ^= num;
         }
+
         return res;
     }
 };
 
+int main() {
+    
+    vector<int> nums = {2, 2, 1};
 
+    Solution obj;
+
+    cout << "Single Number: " << obj.singleNumber(nums);
+
+    return 0;
+}
